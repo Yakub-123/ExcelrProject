0dfc681e89bd2ad54f4495112887bab516e33dff
Yakub-123 <venkysithub@gmail.com>
2024-07-05 19:08:50 +0530

    my task like addition two number and login page completed

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+public static void main(String args[])
+{
+int n1=90;
+int n2=70;
+int add=n1+n2;
+System.out.print("Hello GitHub");
+System.out.print("Addition of two number="+add);
+}
